861d528fb287746129634b3a9ce00a41656dd62f
Ken Lee <kenlee.dev100@outlook.com>
2013-05-09 19:56:53 +0800

    devise error

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,38 +3,38 @@ module Api
     class SessionsController < Api::BaseController
  
       # prepend_before_filter :require_no_authentication, only: [:create]
-      include ::Devise::Controllers::DeviseController
+      # include ::Devise::Controllers::DeviseController
       
-      before_filter :ensure_params_exist
+      # before_filter :ensure_params_exist
      
-      def create
-        build_resource
-        resource = User.find_for_database_authentication(email: params[:user][:email])
-        return invalid_login_attempt unless resource
+      # def create
+      #   build_resource
+      #   resource = User.find_for_database_authentication(email: params[:user][:email])
+      #   return invalid_login_attempt unless resource
      
-        if resource.valid_password?(params[:user][:password])
-          sign_in(:user, resource)
-          render json: { success: true, auth_token: resource.authentication_token, email: resource.email }
-          return
-        end
-        invalid_login_attempt
-      end
+      #   if resource.valid_password?(params[:user][:password])
+      #     sign_in(:user, resource)
+      #     render json: { success: true, auth_token: resource.authentication_token, email: resource.email }
+      #     return
+      #   end
+      #   invalid_login_attempt
+      # end
       
-      def destroy
-        sign_out(resource_name)
-      end
+      # def destroy
+      #   sign_out(resource_name)
+      # end
      
-      protected
+      # protected
   
-        def ensure_params_exist
-          return unless params[:user].blank?
-          render json: { success: false, message: "Missing user parameter" }, status: 422
-        end
+      #   def ensure_params_exist
+      #     return unless params[:user].blank?
+      #     render json: { success: false, message: "Missing user parameter" }, status: 422
+      #   end
        
-        def invalid_login_attempt
-          warden.custom_failure!
-          render json: { success: false, message: "Error with your email or password" }, status: 401
-        end
+      #   def invalid_login_attempt
+      #     warden.custom_failure!
+      #     render json: { success: false, message: "Error with your email or password" }, status: 401
+      #   end
 
     end
   end
