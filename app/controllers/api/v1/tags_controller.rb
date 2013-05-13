ae3cf8ad0d2f198acfd514baa7a1517956019c32
Ken Lee <kenlee.dev100@outlook.com>
2013-05-14 07:10:10 +0800

    fix tag api

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -3,38 +3,52 @@ module Api
     class TagsController < Api::BaseController
       
       before_filter :validate_authentication_token
+      before_filter :ensure_params_id_exist
 
       def show
+        name = params[:id].downcase
         tag = Tag.find_by_name(params[:id])
         return response_for_tag(tag)
       end
 
-      # REFACTOR: tag name "search" becomes reserve word for routes
       def search
-        tag = Tag.find_by_name(params[:id])
-        return response_for_tag(tag) unless tag.present?
+        name = params[:id].downcase
+        tags = Tag.where("name LIKE ?", name + '%').order("name ASC")
 
-        render json: tag.to_builder(true).target!, status: 200
+        json = Jbuilder.encode do |json|
+          json.data do |data|
+            data.tags tags, :name, :posts_count
+          end
+          json.success true
+        end
+
+        render json: json, status: 200
       end
 
       def recent
-        tag = Tag.find_by_name(params[:id])
+        name = params[:id].downcase
+        tag = Tag.find_by_name(name)
         return response_for_tag(tag) unless tag.present?
 
         hash_conditions = {}
         hash_conditions.update(min_id: params[:min_id]) if params[:min_id].present?
         hash_conditions.update(max_id: params[:max_id]) if params[:max_id].present?
-        
+
         render json: tag.to_builder(true, hash_conditions).target!, status: 200
       end
 
       protected
 
+        def ensure_params_id_exist
+          return unless params[:id].blank?
+          render json: { success: false, message: "Missing tagname parameter" }, status: 200
+        end
+
         def response_for_tag(tag)
           if tag.present?
             render json: tag.to_builder.target!, status: 200
           else
-            render json: { success: false, message: "Tag not found" }, status: 200
+            render json: { success: false, message: "Tagname not found" }, status: 200
           end
         end
     end
