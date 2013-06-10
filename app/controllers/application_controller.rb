c8eed887197227a25f10135e1b9f5ebbe8f57250
Ken Lee <kenlee.dev100@outlook.com>
2013-06-10 17:47:27 +0800

    change after_sign_in_path_for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def after_sign_in_path_for(resource_or_scope)
+    case resource_or_scope
+    when :admin, Admin
+      admin_index_path
+    else
+      super
+    end
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     case resource_or_scope
     when :admin, Admin
