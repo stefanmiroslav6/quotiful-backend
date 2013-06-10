311a358165d01e09ea78adbe51cb77160e2dd1d7
Ken Lee <kenlee.dev100@outlook.com>
2013-06-10 08:33:38 +0800

    aesthetics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_out_path_for(resource_or_scope)
+    case resource_or_scope
+    when :admin, Admin
+      new_admin_session_path
+    else
+      super
+    end
+  end
 end
