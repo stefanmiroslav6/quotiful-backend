1e94e90425b76c7f1ff1098c68b62413624df0cc
Ken Lee <kenlee.dev100@outlook.com>
2013-06-10 18:05:54 +0800

    added default image

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -5,3 +5,8 @@ app.configure_with(:imagemagick)
 app.configure_with(:rails)
 
 app.define_macro(ActiveRecord::Base, :image_accessor)
+
+Dragonfly[:images].configure do |c|
+  c.allow_fetch_file = true
+  c.protect_from_dos_attacks = true
+end
