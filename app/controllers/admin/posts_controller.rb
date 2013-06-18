1e0723a0f01fc9476b96626083a98ba07a64ca3a
Ken Lee <kenlee.dev100@outlook.com>
2013-06-19 03:13:38 +0800

    fix for sort by time

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -2,7 +2,32 @@ class Admin::PostsController < AdminController
   def index
     condition = params[:sort].present? and params[:sort].in?(%(editors_pick likes_count))
     sort = condition ? params[:sort].dup : 'created_at'
-    @posts = Post.order("#{sort} DESC, created_at DESC").page(params[:page]).per(20)
+    
+    if sort.eql?('likes_count')
+      case params[:by]
+      when 'today' then start_date = Time.zone.now.midnight
+      when 'yesterday' then start_date = Time.zone.now.yesterday.midnight
+      when 'last_week'
+        start_date = Time.zone.now.prev_week
+        end_date = Time.zone.now.prev_week.end_of_week
+      when 'this_month' then start_date = Time.zone.now.beginning_of_month
+      when 'this_year' then start_date = Time.zone.now.beginning_of_year
+      else
+        end_date = Time.zone.now
+      end
+    end
+    
+    posts = Post.order("#{sort} DESC, created_at DESC").page(params[:page]).per(20)
+    
+    if end_date.present? 
+      posts = posts.where("created_at <= ?", end_date)
+    end
+
+    if start_date.present?
+      posts = posts.where("created_at >= ?", start_date)
+    end
+    
+    @posts = posts
   end
 
   def update
