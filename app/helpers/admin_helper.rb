55a620157415b8352eeff9f6d409ed2ddca77a2a
Ken Lee <kenlee.dev100@outlook.com>
2013-06-25 19:24:16 +0800

    added sort arrows for sort by popularity

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,2 +1,10 @@
 module AdminHelper
+  def currently_sorted_by?(expected, actual)
+    str = expected.gsub('_', ' ')
+    if expected.eql?(actual) or (expected.eql?('all_time') and actual.blank?)
+      "#{content_tag(:i, '', :class => 'icon-arrow-down')} #{str}".html_safe
+    else
+      str.html_safe
+    end
+  end
 end
