f033a9a29779fad021e31ed2151d24cce9e69496
Ken Lee <kenlee.dev100@outlook.com>
2013-07-12 19:24:13 +0800

    notifier for collection saves your quotiful

--- app/observers/collection_observer.rb
+++ app/observers/collection_observer.rb
@@ -2,6 +2,10 @@ class CollectionObserver < ActiveRecord::Observer
   def after_create(collection)
     user = collection.user
     user.increment!(:collection_count)
+
+    post = collection.post
+
+    Resque.enqueue(Jobs::Notify, :saves_your_quotiful, post.user_id, user.id, {post_id: post.id})
   end
 
   def before_destroy(collection)
