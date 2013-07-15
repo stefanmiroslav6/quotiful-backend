5f16988196a2e3bd07e0a35cc88f1ffd0c9a78c4
Ken Lee <kenlee.dev100@outlook.com>
2013-07-15 19:24:33 +0800

    fix for incompatible character encodings

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
     @post = Post.find_by_created_at(Time.at(params[:id].to_i))
     @page_title = "Photo by #{@post.user.full_name} • "
     @ogtags = {
-      title: "Photo by #{@post.user.full_name} • Quotiful",
+      title: "Photo by #{@post.user.full_name} | Quotiful",
       description: "#{@post.user.full_name}'s photo on Quotiful",
       web_url: post_url(@post.created_at.to_i),
       image_url: @post.quote_image_url
