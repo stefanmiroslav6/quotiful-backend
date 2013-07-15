3c21f186581d0f82f69b016cf94ecea0d7b2103f
Ken Lee <kenlee.dev100@outlook.com>
2013-07-15 23:31:37 +0800

    fix for missing host of quote_image_url

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,11 +2,13 @@ class PostsController < ApplicationController
   def show
     @post = Post.find_by_created_at(Time.at(params[:id].to_i))
     @page_title = "Photo by #{@post.user.full_name} | "
+    image_path = @post.quote_image_url
+    host = Rails.env.development? ? 'http://localhost:3000' : URI::HTTP.build({host: DEFAULT_HOST})
     @ogtags = {
       title: "Photo by #{@post.user.full_name} | Quotiful",
       description: "#{@post.user.full_name}'s photo on Quotiful",
       web_url: post_url(@post.created_at.to_i),
-      image_url: @post.quote_image_url
+      image_url: host + image_path
     }
     
     unless @post.present?
