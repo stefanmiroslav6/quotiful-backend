01d2b848c6bb3a7541cae03b5c77f9c61c88f4f4
Ken Lee <kenlee.dev100@outlook.com>
2013-07-15 18:50:38 +0800

    added opengraph meta tags

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,16 @@
 class PostsController < ApplicationController
   def show
     @post = Post.find_by_created_at(Time.at(params[:id].to_i))
+    @page_title = "Photo by #{@post.user.full_name} • "
+    @ogtags = {
+      title: "Photo by #{@post.user.full_name} • Quotiful",
+      description: "#{@post.user.full_name}'s photo on Quotiful",
+      web_url: post_url(@post.created_at.to_i),
+      image_url: @post.quote_image_url
+    }
     
     unless @post.present?
-      redirect_to root_url, alert: "No image found."
+      redirect_to root_url, alert: "Photo not found."
       return
     end
   end
