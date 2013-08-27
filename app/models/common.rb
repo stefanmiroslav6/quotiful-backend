2ef282949d44db6fb08bbfd480034a76d105ae0e
Ken Lee <kenlee.dev100@outlook.com>
2013-08-28 02:58:03 +0800

    image image

--- app/models/common.rb
+++ app/models/common.rb
@@ -2,7 +2,8 @@ class Common
 
   def self.image_url(image, size = '')
     if image.present?
-      size.present? ? image.thumb(size).url : image.jpg.url
+      # size.present? ? image.thumb(size).remote_url : image.jpg.remote_url
+      image.remote_url
     else
       path = File.join(Rails.root, 'public', 'default-avatar.png')
       default = Dragonfly[:images].fetch_file(path)
