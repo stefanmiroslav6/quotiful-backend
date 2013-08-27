3eab8ed65eb2e64c365a6fe08580240c067701c9
Ken Lee <kenlee.dev100@outlook.com>
2013-08-28 00:27:26 +0800

    remove unicode removal on posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+unless Rails.env == 'production'
+  jed = User.create(full_name: "Jed Seculles", email: "jeds@local.loc", password: "123123123")
+  nikki = User.create(full_name: "Nikki Fernandez", email: "nikkif@local.loc", password: "123123123")
+  andy = User.create(full_name: "Jonathan Andy Lim", email: "andyl@local.loc", password: "123123123")
+  jedford = User.create(full_name: "Jedford Seculles", email: "jedfords@local.loc", password: "123123123")
+  jedford = User.create(full_name: "Jeff Seculles", email: "jeffs@local.loc", password: "123123123")
 
-jed = User.create(full_name: "Jed Seculles", email: "jeds@local.loc", password: "123123123")
-nikki = User.create(full_name: "Nikki Fernandez", email: "nikkif@local.loc", password: "123123123")
-andy = User.create(full_name: "Jonathan Andy Lim", email: "andyl@local.loc", password: "123123123")
-jedford = User.create(full_name: "Jedford Seculles", email: "jedfords@local.loc", password: "123123123")
-jedford = User.create(full_name: "Jeff Seculles", email: "jeffs@local.loc", password: "123123123")
+  jed.followed_by_self << [nikki, andy]
 
-jed.followed_by_self << [nikki, andy]
+  post1 = nikki.posts.create(quote: "Lorem ipsum dolor", caption: "Lorem ipsum dolor #sit #amet", author_name: "Auctor Convallis")
+  post2 = andy.posts.create(quote: "Lorem ipsum dolor", caption: "Ipsum dolor sit #amet #consectetur #adipiscing", author_name: "Auctor Convallis")
 
-post1 = nikki.posts.create(quote: "Lorem ipsum dolor", caption: "Lorem ipsum dolor #sit #amet", author_name: "Auctor Convallis")
-post2 = andy.posts.create(quote: "Lorem ipsum dolor", caption: "Ipsum dolor sit #amet #consectetur #adipiscing", author_name: "Auctor Convallis")
+  jed.liked_posts << [post1, post2]
 
-jed.liked_posts << [post1, post2]
+  comment1 = post1.comments.create(body: "Donec arcu arcu")
+  comment2 = post2.comments.create(body: "Scelerisque eu commodo euismod")
+  comment3 = post2.comments.create(body: "Molestie ac ipsum")
 
-comment1 = post1.comments.create(body: "Donec arcu arcu")
-comment2 = post2.comments.create(body: "Scelerisque eu commodo euismod")
-comment3 = post2.comments.create(body: "Molestie ac ipsum")
-
-jed.comments << [comment1, comment2, comment3]
+  jed.comments << [comment1, comment2, comment3]
+end
 
 admin = Admin.create(email: 'quotiful01@gmail.com', password: 'quotiful123', password_confirmation: 'quotiful123')
