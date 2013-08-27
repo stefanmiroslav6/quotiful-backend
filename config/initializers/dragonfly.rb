7d171a45ad7cb9aa744522fe42e0861917260933
Ken Lee <kenlee.dev100@outlook.com>
2013-08-27 21:15:12 +0800

    arrrrgh!

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -17,7 +17,7 @@ if Rails.env == 'production'
   app.datastore.configure do |c|
     c.bucket_name = 'nicephore'
     c.access_key_id = 'AKIAJ5PJULN75BYPMHTQ'
-    c.secret_access_key = 'iz0qp0zeUIHelqRb7EjAUafh7U38j5'
+    c.secret_access_key = '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'
     # c.region = 'eu-west-1'                        # defaults to 'us-east-1'
     # c.storage_headers = {'some' => 'thing'}       # defaults to {'x-amz-acl' => 'public-read'}
     # c.url_scheme = 'https'                        # defaults to 'http'
