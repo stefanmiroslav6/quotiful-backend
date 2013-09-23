e05d71484c6e85bcf7d968e523a8a845a9388d28
Ken Lee <kenlee.dev100@outlook.com>
2013-09-23 23:05:04 +0800

    Automatically make quotiful a follower of a new user after signup

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -1,4 +1,11 @@
 class UserObserver < ActiveRecord::Observer
+  def after_create(user)
+    quotiful = User.where(email: 'info@quotiful.com').first
+    if quotiful.present? and quotiful != user
+      user.followed_by_users << quotiful
+    end
+  end
+
   def after_save(user)
     # SOLR: add to solr index
     user.index!
