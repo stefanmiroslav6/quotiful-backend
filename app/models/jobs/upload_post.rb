c4ea0955bc93b44c3d39559a0ad08361cf5ba445
Ken Lee <kenlee.dev100@outlook.com>
2013-10-07 22:51:42 +0800

    added other image sizes

--- app/models/jobs/upload_post.rb
+++ app/models/jobs/upload_post.rb
@@ -11,6 +11,8 @@ module Jobs
       post = Post.find(post_id)
 
       images = Response::Object.new('post', post).to_hash[:data][:post].values_at(:s_thumbnail_url, :m_thumbnail_url, :quote_image_url).delete_if {|u| u.starts_with?("http://d1t4f9gbrjiu98.cloudfront.net/")}
+      images += [post.quote_image_url('35x35#'), post.quote_image_url('612x612#'), post.quote_image_url]
+
       EM.synchrony do
         EM::Synchrony::FiberIterator.new(images, 3).each do |image|
           url = URI.parse(image)
