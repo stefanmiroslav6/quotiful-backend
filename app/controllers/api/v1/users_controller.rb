dab94885b3b4f9142260958723d09ca76d8b9c59
Ken Lee <kenlee.dev100@outlook.com>
2013-12-04 19:53:44 +0800

    added count for followers and following

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -46,8 +46,9 @@ module Api
 
       def follows
         page = params[:page] || 1
+        count = params[:count] || 10
 
-        @users = instance_user.followed_by_self.includes(:follows, :followers).page(page).per(10)
+        @users = instance_user.followed_by_self.includes(:follows, :followers).page(page).per(count)
 
         json = Response::Collection.new('user', @users, { current_user_id: current_user.id, relative_user_id: instance_user.id, page: page }).to_json
         
@@ -56,8 +57,9 @@ module Api
 
       def followed_by
         page = params[:page] || 1
+        count = params[:count] || 10
 
-        @users = instance_user.followed_by_users.includes(:follows, :followers).page(page).per(10)
+        @users = instance_user.followed_by_users.includes(:follows, :followers).page(page).per(count)
 
         json = Response::Collection.new('user', @users, { current_user_id: current_user.id, relative_user_id: instance_user.id, page: page }).to_json
 
@@ -66,8 +68,9 @@ module Api
 
       def requested_by
         page = params[:page] || 1
+        count = params[:count] || 10
         
-        @users = current_user.requested_by_users.page(page).per(10)
+        @users = current_user.requested_by_users.page(page).per(count)
 
         json = Response::Collection.new('user', @users, { current_user_id: current_user.id, relative_user_id: instance_user.id, page: page }).to_json
 
