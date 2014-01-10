6f72d1dd6e5ec2c606cdd257e3c3f93b0a8c4df2
Ken Lee <kenlee.dev100@outlook.com>
2014-01-10 19:48:40 +0800

    fix for remote url thru image job uid

--- app/models/common.rb
+++ app/models/common.rb
@@ -2,7 +2,7 @@ class Common
 
   def self.image_url(image, size = '')
     image_path = if image.present?
-      size.present? ? image.thumb(size).remote_url : image.remote_url
+      size.present? ? image.thumb(size).store : image.job.uid
     else
       path = File.join(Rails.root, 'public', 'default-avatar.png')
       default = Dragonfly.app.fetch_file(path)
@@ -16,14 +16,15 @@ class Common
     if image_path.start_with?('http', 'https') or Rails.env.eql?('development')
       image_path
     else
-      path, _, sha = image_path.split(/[\?\=]/)
-      URI::HTTP.build({
-        host: DEFAULT_HOST,
-        path: path,
-        query: {
-          sha: sha
-        }.to_param
-      }).to_s
+      # path, _, sha = image_path.split(/[\?\=]/)
+      # URI::HTTP.build({
+      #   host: DEFAULT_HOST,
+      #   path: path,
+      #   query: {
+      #     sha: sha
+      #   }.to_param
+      # }).to_s
+      Dragonfly.app.remote_url_for(image_path)
     end
   end
 
