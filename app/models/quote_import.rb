f507544367af585fe91260b5165d1b2bb6f1e513
Ken Lee <kenlee.dev100@outlook.com>
2015-05-28 05:19:17 +0200

    import issue fixed

--- app/models/quote_import.rb
+++ app/models/quote_import.rb
@@ -87,9 +87,9 @@ class QuoteImport
 
   def open_spreadsheet
     case File.extname(file.original_filename)
-    when ".csv" then Roo::Csv.new(file.path)
-    when ".xls" then Roo::Excel.new(file.path)
-    when ".xlsx" then Roo::Excelx.new(file.path)
+    when ".csv" then Roo::Csv.new(file.path, file_warning: :ignore)
+    when ".xls" then Roo::Excel.new(file.path, file_warning: :ignore)
+    when ".xlsx" then Roo::Excelx.new(file.path, file_warning: :ignore)
     else raise "Unknown file type: #{file.original_filename}"
     end
   end
